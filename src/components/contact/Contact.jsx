b31bc1de67bff6a27c0a02dd4d84410377961f19
farwaMuhibzada <farwafarid2017@gmail.com>
2025-01-13 12:14:45 +0430

    push all code of react portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
-
+import {MdOutlineEmail} from "react-icons/md";
+import {BsLinkedin} from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs';
 function Contact() {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touh</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact_option'>
+            <MdOutlineEmail className='contact_option-icon' />
+            <h4>Email</h4>
+            <h5>Farwafarid2017@gmail.com</h5>
+            <a href="mailto:farrwafarid2017@gmail.com">Send a Email</a>
+          </article>
+
+          <article className='contact_option'>
+            <BsLinkedin className='contact_option-icon' />
+            <h4>Linkdin</h4>
+            <h5>Farwa Muhibzada</h5>
+            <a href="https://www.linkedin.com/in/farwamohibzada/" target='__blank'>Join In My Network</a>
+          </article>
+
+          <article className='contact_option'>
+            <BsWhatsapp className='contact_option-icon' />
+            <h4>Whatsapp</h4>
+            <h5>+93782907617</h5>
+            <a href="https://api.whatsapp.com/send?phone+93782907617">Send a Message</a>
+          </article>
+        </div>
+        {/*  EN DOF CONTACT OPTIONS */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="text" name='email' placeholder='Your Email' required />
+           <textarea name="message" rows='7' placeholder='Your Message' required></textarea>
+           <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
