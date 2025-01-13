732f5eaceb22433ba0b07962b0ce0f83fdaba329
farwaMuhibzada <farwafarid2017@gmail.com>
2025-01-13 14:31:52 +0430

    added Cv file .

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ import { FiDribbble } from 'react-icons/fi'
 function HeaderSocials() {
   return (
     <div className="header__socials">
-        <a href="https://linkdin.com" target='__blank'><BsLinkedin /></a>
-        <a href="https://www.linkedin.com/in/farwamohibzada/" target='__blank'><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/farwamohibzada/" target='__blank'><BsLinkedin /></a>
+        <a href="https://github.com/FarwaMuhibZada" target='__blank'><FaGithub /></a>
         <a href="https://dribbble.com" target='__blank'><FiDribbble/></a>
     </div>
   )
